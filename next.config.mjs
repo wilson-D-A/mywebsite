0820a48751d237fd91c162b1bbc65a724314bbd7
Wilson <wilson.diant@gmail.com>
2026-05-16 12:34:34 -0400

    fix: update destination URL in rewrites for project paths

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     return [
       {
         source: "/project/:path*",
-        destination:[ "https://mta-ada-alerts.vercel.app/project/:path*", "watch-later-topaz.vercel.app"] 
+        destination:[ "https://mta-ada-alerts.vercel.app/project/:path*", "watch-later-topaz.vercel.app/project/:path*"] 
         // Proxy to blog app
       },
     ];
