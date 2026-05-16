161d00cdc18139d5a6051f012d807990aff9a556
Wilson <wilson.diant@gmail.com>
2026-05-16 12:53:24 -0400

    fix: update URL rewrites for project paths

--- next.config.mjs
+++ next.config.mjs
@@ -2,12 +2,9 @@
 const nextConfig = {
   async rewrites() {
     return [
+
       {
-        source: "ada/:path*",
-        destination: "https://mta-ada-alerts.vercel.app/project/:path*", // Proxy to blog app
-      },
-      {
-        source: "watch/:path*",
+        source: "projects/:path*",
         destination: "https://watch-later-topaz.vercel.app/project/:path*", // Proxy to blog app
       }
     ];
