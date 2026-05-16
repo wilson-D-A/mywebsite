bf72ae1e7a3c74a51550fda208f1a15725c76094
Wilson <wilson.diant@gmail.com>
2026-05-16 12:54:17 -0400

    fix: correct source path and destination URL in rewrites, fix reactStrictMode typo

--- next.config.mjs
+++ next.config.mjs
@@ -2,14 +2,13 @@
 const nextConfig = {
   async rewrites() {
     return [
-
       {
-        source: "projects/:path*",
-        destination: "https://watch-later-topaz.vercel.app/project/:path*", // Proxy to blog app
-      }
+        source: "/project/:path*",
+        destination: "https://mta-ada-alerts.vercel.app/project/:path*", // Proxy to blog app
+      },
     ];
   },
-  reactStrictsMode: true,
+  reactStrictMode: true,
 };
 
 export default nextConfig;
